fdd03a9ae44da123c095a93e3223d44e9d67e5a4
AveryL <lxylbznb@gmail.com>
2023-09-29 14:14:55 +0100

    Carousel component added(button to be fixed

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { Navbar } from "./Layouts/NavBarAndFooter/Navbar";
 import { ExploreTopBooks } from "./Layouts/HomePage/ExploreTopBooks";
+import { Carousel } from "./Layouts/HomePage/Carousel";
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
+      <Carousel />
     </div>
   );
 }
