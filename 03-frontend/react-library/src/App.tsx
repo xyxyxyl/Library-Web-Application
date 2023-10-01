5f5403859cd7a7bced59a9f499a410f4d80558d8
AveryL <lxylbznb@gmail.com>
2023-10-01 11:01:36 +0100

    switch and routing

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,13 +4,26 @@ import { Navbar } from "./Layouts/NavBarAndFooter/Navbar";
 import { Footer } from "./Layouts/NavBarAndFooter/Footer";
 import { HomePage } from "./Layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./Layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBooksPage />
+      <Switch>
+        <Route path="/" exact>
+          {/* using exact to render only the homepage*/}
+          <Redirect to="home" />
+        </Route>
+        <Route path="/home">
+          {/* using exact to render only the homepage*/}
+          <HomePage />
+        </Route>
+        <Route path="/search">
+          <SearchBooksPage />
+        </Route>
+      </Switch>
+
       <Footer />
     </div>
   );
