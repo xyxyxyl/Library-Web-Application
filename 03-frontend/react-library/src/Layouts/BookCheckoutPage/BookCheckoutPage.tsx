d9f958987cd46e73a04b068f7601bb9e5ab3d517
AveryL <lxylbznb@gmail.com>
2023-10-09 16:44:59 +0100

    cheout page updated

--- 03-frontend/react-library/src/Layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/Layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BookModel from "../../Models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { StarReview } from "../Utils/StarReview";
+import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
 
 export const BookCheckoutPage = () => {
   const [book, setBook] = useState<BookModel>();
@@ -80,6 +81,7 @@ export const BookCheckoutPage = () => {
               <StarReview rating={4} size={32} />
             </div>
           </div>
+          <CheckoutAndReviewBox book={book} mobile={false} />
         </div>
 
         <hr />
@@ -107,6 +109,7 @@ export const BookCheckoutPage = () => {
             <StarReview rating={4} size={32} />
           </div>
         </div>
+        <CheckoutAndReviewBox book={book} mobile={true} />
 
         <hr />
       </div>
