ef6c65fbb0fa38fb86fa93eb6432d2ce118bec60
AveryL <lxylbznb@gmail.com>
2023-11-13 10:47:24 +0000

    implement review list page

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -10,6 +10,7 @@ import { oktaConfig } from "./lib/oktaConfig";
 import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
 import { Security, LoginCallback } from "@okta/okta-react";
 import LoginWidget from "./Auth/LoginWidget";
+import { ReviewListPage } from "./Layouts/BookCheckoutPage/ReviewListPage/ReviewListPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -42,6 +43,10 @@ export const App = () => {
               {/* using exact to render only the homepage*/}
               <HomePage />
             </Route>
+            <Route path="/reviewlist/:bookId">
+              <ReviewListPage />
+            </Route>
+
             <Route path="/search">
               <SearchBooksPage />
             </Route>
