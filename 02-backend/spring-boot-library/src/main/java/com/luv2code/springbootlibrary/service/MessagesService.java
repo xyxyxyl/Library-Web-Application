f05277e05363e92ccd3076f83f02b3df4cab0341
AveryL <lxylbznb@gmail.com>
2023-11-21 22:30:08 +0000

    admin message configuration

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/MessagesService.java
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/MessagesService.java
@@ -3,9 +3,13 @@ package com.luv2code.springbootlibrary.service;
 
 import com.luv2code.springbootlibrary.DAO.MessageRepository;
 import com.luv2code.springbootlibrary.entity.Message;
+import com.luv2code.springbootlibrary.requestmodels.AdminQuestionRequest;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -21,5 +25,17 @@ public class MessagesService {
         Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messageRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception {
+        Optional<Message> message = messageRepository.findById(adminQuestionRequest.getId());
+        if (!message.isPresent()) {
+            throw new Exception("Message not found");
+        }
+        message.get().setUserEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
+
     }
 }
