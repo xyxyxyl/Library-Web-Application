fed7f4828701be6e573f7928176771a1d6975fec
AveryL <lxylbznb@gmail.com>
2024-11-03 09:53:15 +0000

    updated

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/DAO/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springbootlibrary.DAO;
-
-
-import com.luv2code.springbootlibrary.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Payment findByUserEmail(String userEmail);
-}
